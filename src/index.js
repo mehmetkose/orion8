ae3525a9affe43d336a3dce6629b00e6929ef34e
Mehmet Kose <mehmetkose122@gmail.com>
2018-05-23 14:27:56 +0300

    change chain name

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 import blockchain from './blockchain'
 import server from './p2p.js'
 
-const testChain = new blockchain.Chain()
-testChain.addBlock(new blockchain.Block(1, Date.now(), {"hello":"world2"}))
-testChain.addBlock(new blockchain.Block(2, Date.now(), {"hello":"world3"}))
+const orion = new blockchain.Chain()
+orion.addBlock(new blockchain.Block(1, Date.now(), {"hello":"world2"}))
+orion.addBlock(new blockchain.Block(2, Date.now(), {"hello":"world3"}))
 
 console.log("List Blocks:")
-for (let block of testChain.chain) {
+for (let block of orion.chain) {
     console.log(JSON.stringify(block))
 }
 
-console.log("Chain is Valid: " + testChain.isChainValid())
+console.log("Chain is Valid: " + orion.isChainValid())
 
 var p2p_port = process.env.P2P_PORT || 38746;
-const websocketServer = new server.Server(testChain)
+const websocketServer = new server.Server(orion)
 websocketServer.startServer(p2p_port);
 console.log('listening websocket p2p port on: ' + p2p_port)
